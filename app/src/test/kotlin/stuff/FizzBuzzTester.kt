d5fa6b76504e97b3a5437236b8380f1f5db2d55f
Jan Vermeir <jvermeir@hotmail.com>
2022-02-02 21:02:39 +0100

    loop test

--- app/src/test/kotlin/stuff/FizzBuzzTester.kt
+++ app/src/test/kotlin/stuff/FizzBuzzTester.kt
@@ -66,7 +66,7 @@ class FizzBuzzTester {
     @Test
     fun fizzBuzzSanityCheck() {
         val expectedResults = arrayOf(
-            "x", "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14",
+            "justIgnoreThisOK?", "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14",
             "FizzBuzz", "16", "17", "Fizz", "19", "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26",
             "Fizz", "28", "29", "FizzBuzz"
         )
